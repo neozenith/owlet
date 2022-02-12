7e912e88ee3838af9e41fc469ea60865eb730e9c
Josh Peak <neozenith.dev@gmail.com>
2022-02-12 14:10:38 +1100

    Make data model actually a form

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -3,13 +3,30 @@ import React from 'react'
 const Form = (props: any) => {
     const fields = props.fields as Array<{name: string, type: string}>;
 
+    const inputTypeForFieldType = (fieldType: string): string => {
+        return "text"
+    };
+
     return (
         <div>
             <h3>{props.name}</h3>
             <div>{props.description}</div>
-            <ul>
-                { fields.map((field, index) => <li key={index}>{field.name}: {field.type}</li>) }
-            </ul>
+            <form>
+                
+            {
+                fields.map((field, index) => {
+                    return(
+                        <div>
+                        <label>{field.name}</label>
+                        <input key={index} id={field.name} value="" type={inputTypeForFieldType(field.type)} />
+                        </div>
+                    );
+                })
+            }
+
+            <input type="submit" />
+                
+            </form>
 
         </div>
     )
